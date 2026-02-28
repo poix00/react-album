e8fcf3131ea161fab946ef70c7b99635aa05439b
poix00 <poix00@gmail.com>
2026-02-28 12:45:26 +0900

    faeat: Initial commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,26 @@
+import styles from './styles/index.module.scss'
+
+function index() {
+    return (
+    <div className={styles.page}>
+       {/* {공통헤더 UI 부분} */}
+       {/* {공통 네비게이션 UI 부분} */}
+        <div className={styles.page_contents}>
+            <div className={styles.page_contents_introBox}>
+                <div className={styles.wrapper}>
+                    <span className={styles.wrapper_title}>PhtoSplash</span>
+                    <span className={styles.wrapper_desc}>
+                        인터넷의 시각 자료 출처입니다 <br />
+                        모든 지역에 있는 크리에이터들의 지원을 받습니다.
+                    </span>
+                    {/* {검색창 UI 부분} */}
+                </div>
+            </div>
+            <div className={styles.page_contents_imageBox}></div>
+        </div>
+        {/* 공통 푸터 UI 부분 */}
+    </div>
+    )
+}
+
+export default index
